1e461c8a431364b01b13a12341b3a47a3d1aaa4f
KorablinS <korablin198421@gmail.com>
2022-09-29 20:53:04 +0300

    Implemented openText in MainActivity

--- app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
+++ app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -52,10 +54,37 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
         }
+    }
+    // открытие файла
+    public void openText(View view){
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(R.id.text);
+
+            try {
+                fin = openFileInput(FILE_NAME);
+                byte [] bytes = new byte[fin.available()];
+                fin.read(bytes);
+                String text = new String(bytes);
+                textView.setText(text);
 
 
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try{
+                    if(fin != null){
+                        fin.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 
+                }
 
+            }
     }
 
 }
